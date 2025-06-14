1ebd4ef366b2209539635abbf7aa79e6505e0521
Ethan Dodgen <thefivalco@gmail.com>
2025-06-14 08:15:32 -0500

    update

--- dropbox-backend/server.js
+++ dropbox-backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const dropboxRoutes = require('./dropbox');
+const dropboxRoutes = require('../src/api/dropbox');
 
 const app = express();
 const PORT = 3000;
