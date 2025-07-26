a4607ce592907135fca8fb3cb90d73311784c6f3
Ryan Blessing <rblessing@iprosystems.com>
2025-07-26 14:51:06 -0500

    css changes

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,12 +2,12 @@
 <template>
   <v-container fluid>
     <div class="contact-info">
-      <p style="font-size: 1.3rem; color: #42b983"> For custom murals and project inquires</p>
-      <p style="font-size: 1.3rem; color: #42b983">please email us at: </p>
+      <p  style="font-size: 1.3rem; color: #42b983; font-family: var(--font-gotham);"> For custom murals, signs, goldleaf, or other project inquires</p>
+      <p style="font-size: 1.3rem; color: #42b983; font-family: var(--font-gotham);">please email us at: </p>
         <div class="email-container">
           <v-icon>mdi-email</v-icon>
           <h3>
-            <a href="mailto:cannedpineappleco.com" style="color: #72a2e4; font-size: 2rem; text-decoration: none;">
+            <a href="mailto:cannedpineappleco.com" style="font-family: var(--font-gotham); color: #72a2e4; font-size: 2rem; text-decoration: none;">
               cannedpineappleco.com
             </a>
           </h3>
@@ -35,7 +35,7 @@ export default {
   align-items: center;
   text-align: center;
   min-height: 60vh;
-  font-family: 'Gotham', sans-serif;
+  font-family: var(--font-gotham);
   /* margin: 0 auto;
   padding: 10px; */
 }
